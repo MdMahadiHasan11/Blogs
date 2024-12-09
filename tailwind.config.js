bcf58858216948cbd092acbebad797742524cc68
MdMahadiHasan11 <hasanpustcse11@gmail.com>
2024-12-10 00:46:47 +0600

    Describe the changes you made

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
+// Import plugins using ES module syntax
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-
   theme: {
     extend: {},
   },
   plugins: [
-    require('daisyui'),
-    ],
-}
-
+    daisyui, // Use the imported plugin
+  ],
+};
