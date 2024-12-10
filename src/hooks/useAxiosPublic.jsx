94056c77cb28c795997942dd5d52683e7fa5544a
MdMahadiHasan11 <hasanpustcse11@gmail.com>
2024-12-10 10:14:24 +0600

    updatede and connect server

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    // baseURL:'https://medicine-world-server.vercel.app'https://assigment-server-sigma.vercel.app/
-    baseURL:'https://assigment-server-sigma.vercel.app'
+    // baseURL:'https://assigment-server-3aro38483-mahadi-hasans-projects-4d0eced2.vercel.app'
+    baseURL:'https://blogs-server-gamma.vercel.app'
 })
 const useAxiosPublic = () => {
     return axiosPublic;
